cbe4e7dac0996be411e8b32033282671acdcc695
Taka005 <01taka005@gmail.com>
2022-09-18 23:07:36 +0900

    修正

--- script.js
+++ script.js
@@ -65,6 +65,7 @@ async function main(){
           legend: {
               position: "left",
           },
+          maintainAspectRatio: false,
           responsive: true,
           scale: {
             pointLabels: {
